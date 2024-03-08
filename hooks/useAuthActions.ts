11a0585bf52f1db1b211ab1ff84066be0ced75c4
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 15:34:44 +0900

    코드수정 useMemo를 사용하도록 수정하였습니다.

--- hooks/useAuthActions.ts
+++ hooks/useAuthActions.ts
@@ -1,8 +1,12 @@
 import {bindActionCreators} from 'redux';
 import {useDispatch} from 'react-redux';
 import {authorize, logout} from '../slices/auth';
+import { useMemo } from 'react';
 
 export default function useAuthActions() {
   const dispatch = useDispatch();
-  return bindActionCreators({authorize, logout}, dispatch);
+  return useMemo(
+    () => bindActionCreators({authorize, logout}, dispatch),
+    dispatch,
+   ]);
 }
