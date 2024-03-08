8e336057514f84887ebebbe75d03c0f1b91361a6
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 20:08:27 +0900

    코드생성 todosState 외에도 selector를 사용하여 nextTodId를 만들어주었습니다.

---
+++ atom/todos.ts
@@ -0,0 +1,26 @@
+import { atom, selector } from 'recoil';
+
+// 추후 컴포넌트에서 불러와야 하기 때문에 export
+export interface Todo {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+export const todosState = atom<Todo[]>({
+  key: 'todosState',
+  default: [
+    {id: 1, text: '리액트 네이티브 배우기', done: true},
+    {id: 2, text: '상태 관리 배우기', done: false},
+  ],
+});
+
+export const nextTodoId = selector({
+  key: 'nextTodoId',
+  get: ({get}) => {
+    const todos = get(todosState);
+    // 마지막 항목의 id를 조회하고 만약 값이 존재하지 않으면 0을 사용
+    const lastId = todos[todos.length - 1]?.id ?? 0;
+    return lastId + 1; // lastId에서 1을 더한 값을 새로운 항목의 id로 사용
+  },
+});
