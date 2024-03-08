611a9afc1d5f78de2895eeb999caffb2e22c2a2c
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 18:24:14 +0900

    코드수정 createStore 대신 configureStore를 사용하도록 하였습니다.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import TodoApp from './components/TodoApp';
 import rootReducer from './slices';
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(rootReducer);
+const store = configureStore({reducer: rootReducer});
 
 function App() {
   return (
