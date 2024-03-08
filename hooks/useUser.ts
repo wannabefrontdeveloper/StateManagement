c2cda5aeeeaf3a700b40daefb8a44aed9aa1b593
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 19:58:10 +0900

    코드수정 useRecoilValue를 사용하도록 수정하였습니다.

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,5 +1,7 @@
-import {useSelector} from 'react-redux';
+import { useRecoilValue } from 'recoil';
+import { authState } from '../atom/auth';
 
 export default function useUser() {
-  return useSelector(state => state.auth.user);
+  const auth = useRecoilValue(authState);
+  return auth.user;
 }
