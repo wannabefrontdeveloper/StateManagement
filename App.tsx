0d5f5a15172e9aa2866d683c7381cf227a7d68c8
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 19:55:01 +0900

    코드수정 리덕스 관련 코드를 지우고 AuthApp 컴포넌트를 RecoilRoot 컴포넌트에 감싸서 렌더링하도록하였습니다.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import rootReducer from './slices';
-import { configureStore } from '@reduxjs/toolkit';
-import PostsApp from './components/PostsApp';
-
-const store = configureStore({reducer: rootReducer});
+import { RecoilRoot } from 'recoil';
+import AuthApp from './components/AuthApp';
 
 function App() {
   return (
-    <Provider store={store}>
-      <PostsApp />
-    </Provider>
+    <RecoilRoot>
+      <AuthApp />
+    </RecoilRoot>
   );
 }
 
