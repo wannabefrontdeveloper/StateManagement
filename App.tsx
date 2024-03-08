8e16662aa7132c19292082022047a16c520b95e9
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 17:44:11 +0900

    코드수정 AuthApp을 지우고 TodoApp을 사용하도록 하였습니다.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import TodoApp from './components/TodoApp';
 import rootReducer from './slices';
-import AuthApp from './components/AuthApp';
 
 const store = createStore(rootReducer);
 
 function App() {
   return (
     <Provider store={store}>
-      <AuthApp />
+      <TodoApp />
     </Provider>
   );
 }
